697ae0f6cfc268da2c5ef8efa4f669ce2ea45b39
PontusSven <pontus.svensson92@gmail.com>
2020-10-29 10:45:21 +0100

    google OAuth initialized

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "161835104526-b5mh2kfll2sm6r90ru36bri3ea7hq7dp.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
